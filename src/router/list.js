ceb1454a6f1ef4ad8f0a158a7e77741d32dbfb14
Sianglife <sianglifen@gmail.com>
2022-11-22 22:28:25 +0800

    fix wrong root path

--- src/router/list.js
+++ src/router/list.js
@@ -1,6 +1,6 @@
 export const home=[
     {
-        "path":"/WebPageToolsBeta2.0",
+        "path":"/WebPageTools2.0",
         "title":"首頁",
         "name":'index',
         "component":()=>require('@/views/homeIndex.vue')
@@ -9,7 +9,7 @@ export const home=[
 export const math=[
     {
         "CommingSoom":false,
-        "path":"/WebPageToolsBeta2.0/math/Factor",
+        "path":"/WebPageTools2.0/math/Factor",
         "title":"質數/因數工具",
         "name":'mathFactor',
         "description":'找質數、因數、質因數與公因數',
@@ -17,7 +17,7 @@ export const math=[
     },
     {
         "CommingSoom":false,
-        "path":"/WebPageToolsBeta2.0/math/Random",
+        "path":"/WebPageTools2.0/math/Random",
         "title":"抽籤",
         "name":'mathRandom',
         "description":'隨機抽籤工具',
@@ -28,7 +28,7 @@ export const math=[
 export const chemistry=[
     {
         "CommingSoom":false,
-        "path":"/WebPageToolsBeta2.0/chemistry/Density",
+        "path":"/WebPageTools2.0/chemistry/Density",
         "title":"密度工具",
         "name":'chemistryDensity',
         "description":'密度、質量、體積換算',
@@ -36,7 +36,7 @@ export const chemistry=[
     },
     {
         "CommingSoom":false,
-        "path":"/WebPageToolsBeta2.0/chemistry/MoleCular",
+        "path":"/WebPageTools2.0/chemistry/MoleCular",
         "title":"分子量計算器",
         "name":'chemistryMoleCular',
         "description":'計算化合物分子量',
