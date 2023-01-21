7b7e99c73c96917072e8c5c4cff813359834a3e7
Sianglife <88283637+Sianglife@users.noreply.github.com>
2023-01-21 13:47:58 +0800

    react try

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { Nav, Navbar, Container } from 'react-bootstrap';
+
+const navUrls= [        
+  {text:'飛翔部落格',href:'https://sianglife.github.io/blog/',icon:'bi bi-pencil',blanktab:true},
+  {text:'小工具首頁',href:'/webpagetools',icon:'bi bi-house-door',blanktab:false}
+]
+
+const renderNavItem = (urls) => {
+  let navItem = [];
+  for(let item of urls){
+    navItem.push(
+      <Nav.Item key={item.text}>
+        <Nav.Link as="a" href={item.href} target={item.blanktab ? '_blank' : '_self'}/>
+      </Nav.Item>
+    )
+  }
+  return navItem;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Navbar bg="light" expand="lg" style={{background: 'rgb(89,154,143)', height: '5em'}}>
+    <Navbar.Brand as="a" href="/">
+      <span>飛翔小工具</span>
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Container>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto">
+          {renderNavItem(navUrls)}
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
   );
 }
 
